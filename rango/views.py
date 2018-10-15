9e90aeefcb031e1105da7d645b21f701c9334af1
Filipi <filipi20rodrigues@gmail.com>
2018-10-14 21:28:37 -0300

    banco de dados concluido

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 #from django.http import HttpResponse# pra mandar uma mensagem sem o template escrevendo direto em HTML
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
